d31f6fa404c52b7e69bf966499526ce173220248
Jorge Monroy <jlmonroy13@gmail.com>
2017-03-16 14:59:43 -0500

    Adding model react module and creating authentications markup

---
+++ src/components/TextFieldGroup.js
@@ -0,0 +1,36 @@
+import React, { PropTypes } from 'react';
+
+const TextFieldGroup = ({ field, value, label, error, type, placeholder, onChange }) => {
+	return (
+		<div>
+			<label htmlFor={field} >{label}</label>
+			<input
+				value={value}
+				onChange={onChange}
+				type={type}
+				name={field}
+				id={field}
+				min="1"
+				className="main-form__input"
+				placeholder={placeholder}
+			/>
+			{error && <span>{error}</span>}
+		</div>
+	);
+};
+
+TextFieldGroup.propTypes = {
+	field: PropTypes.string.isRequired,
+	value: PropTypes.string.isRequired,
+	label: PropTypes.string.isRequired,
+	error: PropTypes.string,
+	type: PropTypes.string.isRequired,
+	placeholder: PropTypes.string,
+	onChange: PropTypes.func.isRequired
+};
+
+TextFieldGroup.defaultProps = {
+	type: 'text'
+};
+
+export default TextFieldGroup;
