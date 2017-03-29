2901e29b51a6dd59df5faf08de7839eb255498b2
Selvio Perez <selvio89@hotmail.com>
2017-03-29 12:04:44 -0500

    Add upload image form

--- src/components/admin/SolutionManualDetail.js
+++ src/components/admin/SolutionManualDetail.js
@@ -1,15 +1,22 @@
 import React, {Component, PropTypes} from 'react';
 import BOOKS from '../../constants/fakeData';
 import { browserHistory } from 'react-router';
+import * as firebase from 'firebase';
 
 class SolutionManualDetail extends Component {
   constructor() {
     super();
     this.state = {
-      solutionManual: {}
-    };
+      solutionManual: {},
+      imageFile: '',
+      chapter: '',
+      subchapter: '',
+      exercise: '',
+    }
 
     this.renderChapters = this.renderChapters.bind(this);
+    this.onSubmitUploadForm = this.onSubmitUploadForm.bind(this);
+    this.onChangeFileInput = this.onChangeFileInput.bind(this);
   }
 
   componentWillMount() {
@@ -49,11 +56,54 @@ class SolutionManualDetail extends Component {
     }
   }
 
+  onSubmitUploadForm(e) {
+    e.preventDefault();
+    const { imageFile, chapter, subchapter, exercise } = this.state;
+    const storageRef = firebase.storage().ref(`d13VdWtP3XKpYiuu9xajqS7HVC3/${imageFile.name}`);
+    const task = storageRef.put(imageFile);
+    task.on('state_changed',
+      function complete(response) {
+        const imageUrl = response.a && response.a.downloadURLs[0];
+        const databasePath = `solutionManuals/d13VdWtP3XKpYiuu9xajqS7HVC3/chapters/${chapter-1}/subchapters/${subchapter-1}/exercises/${exercise-1}`;
+        firebase.database().ref(databasePath).set({ 
+          imageUrl,
+          number: exercise
+        });
+      },
+      function error(err) {
+        console.error(err);
+      }
+    )
+  }
+
+  onChangeFileInput(e) {
+    e.preventDefault();
+    const imageFile = e.target.files[0];
+    this.setState(getDataFromImageFile(imageFile));
+    function getDataFromImageFile(imageFile) {
+      const name = imageFile.name.substr(0, imageFile.name.lastIndexOf('.'));
+      const array = name.split('-');
+      const data = {
+        imageFile,
+        chapter: array[0],
+        exercise: array[array.length - 1],
+      }
+      data.subchapter = array.length === 2 ? null : array[1];
+      return data
+    }
+  }
+
   render() {
     const { solutionManual } = this.state;
     return (
       <div className="container">
         <h1>{this.state.solutionManual.name}</h1>
+        <div className="push--bottom">
+          <form onSubmit={this.onSubmitUploadForm}>
+            <input type="file" className="push-half--right" onChange={this.onChangeFileInput} />
+            <button>Cargar Imagenes</button>
+          </form>
+        </div>
         <table className="table">
           <thead>
             <tr>
